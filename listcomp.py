fde666f0b5ea6cf872d869b5868493b7a9686dc8
chinky garg <chigarg@gmail.com>
2019-01-22 11:59:25 +0530

    assignmnet

---
+++ listcomp.py
@@ -0,0 +1,6 @@
+n = [1,2,3,4,5,68,98,87,96]
+new_list = []
+for i in n:
+	if i%2==0:
+		new_list.append(i)
+		print(i)
---
+++ palindrome.py
@@ -0,0 +1,5 @@
+string=input("enter the string")
+if (string == string[::-1]):
+         print ("entered string is palindrome")
+else:
+         print ("entered string is not palindrome")
